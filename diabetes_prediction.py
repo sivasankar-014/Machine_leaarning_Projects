d42e0353b9c69537c0afa6d6a9656ad0cd177b41
Sivasankar <sivasac444@gmail.com>
2026-06-25 10:13:08 +0530

    remove prime

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -67,15 +67,3 @@
 # check_accuracy(x,y)
   
   
-n=12
-
-if(n>1):
-    for i in range(2,n):
-        if n % i == 0:
-            print(n,' is not a prime number')
-            break
-    else:
-         print(n,'is a prime no')
-else:
-    print(n,'is not a prime number')
-    
